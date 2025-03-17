330edab67460031794d8db580000328503b10666
ksm <hpk5802@github.com>
2025-03-17 23:02:53 +0900

    fix: toast 여러 번 호출되는 이슈 수정 - showToast가 이미 호출된 경우 중복 호출하지 않도록 isToastShown State 추가

--- src/app/hooks/useRedirectIfNotFound.ts
+++ src/app/hooks/useRedirectIfNotFound.ts
@@ -1,16 +1,26 @@
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
+import useToast from './useToast';
 
 const useRedirectIfNotFound = (isNotFound: boolean) => {
   const router = useRouter();
   const [isRedirecting, setIsRedirecting] = useState(false);
+  const [isToastShown, setIsToastShown] = useState(false);
+  const { showToast } = useToast();
 
   useEffect(() => {
     if (isNotFound) {
-      setIsRedirecting(true);
-      router.replace('/notfound');
+      if (!isToastShown) {
+        setIsRedirecting(true);
+        router.replace('/notfound');
+        showToast({
+          message: '유효하지 않은 페이지입니다👀',
+          type: 'error',
+        });
+        setIsToastShown(true);
+      }
     }
-  }, [isNotFound, router]);
+  }, [isNotFound, router, showToast, isToastShown]);
 
   return { isRedirecting };
 };
